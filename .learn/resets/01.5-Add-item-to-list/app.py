c44906fe1f8fcf7b6baa16fb1ff3cf33f003bd33
PabloMorena <pablomorenaserda@gmail.com>
2025-03-17 10:31:11 +0000

    ejercicios pendientes

--- .learn/resets/01.5-Add-item-to-list/app.py
+++ .learn/resets/01.5-Add-item-to-list/app.py
@@ -3,7 +3,3 @@
 my_list = [4, 5, 734, 43, 45]
 
 # The magic goes below
-import random
-for i in range(1, 11):
-    my_list.append(random.randint(1,100))
-    print(my_list)
--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,3 +1,11 @@
 my_list = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 # Your code here
+def sum_odds(list):
+    suma_imp = 0
+    for number in list:
+        if number % 2 !=0:
+            suma_imp += number
+    return suma_imp
+
+sum_odds(my_list)
